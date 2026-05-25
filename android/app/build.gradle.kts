d487d58a2c6ab4e7bf578094af1f54778dfd6b0b
Academykw <cadetkw40@gmail.com>
2026-05-25 11:27:40 +0100

    Finalize .gitignore rules and update project configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.deen.islamic.quiz.deen_iq"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,10 +24,11 @@ android {
         applicationId = "com.deen.islamic.quiz.deen_iq"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
